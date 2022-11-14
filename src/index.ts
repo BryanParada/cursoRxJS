31df84dd18ca4f7cf0445518fb6eff7a4ec681d8
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-14 17:29:33 -0300

    R13 - First Observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import { Observable } from 'rxjs';
 
+const obs$  = new Observable<string>( subs => {
 
-console.log('Hola Mundo!!!');
+    subs.next('Hello')
+    subs.next('World')
 
+    subs.complete();
 
+    subs.next('This line wont appear after subs.complete')
 
+});
 
-
-
-
+obs$.subscribe( console.log );
