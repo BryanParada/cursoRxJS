84405bcf773ab9349464b2012df48d861176a9e1
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-14 18:14:06 -0300

    R16 - Subscription - unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,37 @@ const observer: Observer<any> = {
     next: value => console.log('[Next]: ', value),
     error: error => console.warn('[Error]', error),
     complete: () => console.info('[Completed!]')
-}
+};
 
-const obs$  = new Observable<string>( subs => {
 
-    subs.next('Hello')
-    subs.next('World')
+const insterval$ = new Observable<number>( subs => {
 
-    //forced Error
-    // const a = undefined;
-    // a.name = 'Bryan';
+    let num = 0
 
-    subs.complete();
+    //crear un contador, 1,2,3,4,5....
+    const interval = setInterval( () =>{
+        subs.next(num ++);
+        console.log('num in interval is: ',num);
+        
+    }, 1000);
 
-    subs.next('This line wont appear after subs.complete')
+    return () =>{
+        clearInterval(interval);
+        console.log('Interval destroyed');
+        
+    }
 
 });
-//--------------1
-// obs$.subscribe( console.log );
-//--------------2
-// obs$.subscribe( resp =>{
-//     console.log(resp); 
-// } );
 
-//--------------3
-// obs$.subscribe(
-//     value => console.log('next: ', value),
-//     error => console.warn('error:', error),
-//     () => console.info('Completed')
+const subscription1 = insterval$.subscribe( num => console.log('Num:', num) );
+const subscription2 = insterval$.subscribe( num => console.log('Num:', num) );
+const subscription3 = insterval$.subscribe( num => console.log('Num:', num) );
+
+setTimeout(() =>{
+    subscription1.unsubscribe()
+    subscription2.unsubscribe()
+    subscription3.unsubscribe()
+
+    console.log('Time out Completed');
     
-// );
-
-obs$.subscribe( observer );
+}, 3000);
